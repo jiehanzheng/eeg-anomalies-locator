b01ab627c35f60b7152ac408d6d5a20cfa02f859
Jiehan Zheng <zheng@jiehan.org>
2016-04-11 17:12:04 -0400

    ability to batch process EEG files

--- autofind.py
+++ autofind.py
@@ -1,57 +1,67 @@
+from __future__ import print_function
 import sys
 import eegtools
-import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.svm import OneClassSVM
 
 CONTAMINATION=0.01
 
-print "Loading EEG..."
-eeg = eegtools.io.load_edf(open(sys.argv[1]))
+def process_file(filename):
+  print('='*80)
+  print(filename)
+  print('='*80)
 
-def find_anomaly(label1, label2, winsize):
-  print "Find anomaly in channel", label1 + '-' + label2 + '...'
+  print("Loading EEG", filename, "...", file=sys.stderr)
+  eeg = eegtools.io.load_edf(open(filename))
 
-  # find difference
-  electrode1 = eeg.chan_lab.index(label1)
-  electrode2 = eeg.chan_lab.index(label2)
-  wave = eeg.X[electrode1] - eeg.X[electrode2]
+  def find_anomaly(label1, label2, winsize):
+    print("Find anomaly in channel", label1 + '-' + label2 + '...', file=sys.stderr)
+    print("-"*80)
+    print("Channel [" + label1 + '-' + label2 + ']')
+    print("-"*80)
 
-  # import random
-  # wave = [random.uniform(-20,20) for _ in range(400*30)] + [random.uniform(-2000,2000) for _ in range(5*30)]
-  # wave = np.array(wave)
+    # find difference
+    electrode1 = eeg.chan_lab.index(label1)
+    electrode2 = eeg.chan_lab.index(label2)
+    wave = eeg.X[electrode1] - eeg.X[electrode2]
 
-  print "Splitting into windows..."
-  wave_windows = np.array_split(wave, len(wave)/eeg.sample_rate/winsize)
-  # wave_windows = np.array_split(wave, len(wave)/winsize)
+    # # import random
+    # wave = [random.uniform(-20,20) for _ in range(400*30)] + [random.uniform(-2000,2000) for _ in range(5*30)]
+    # wave = np.array(wave)
 
-  print "Extracting features..."
-  def extract_features(wave_window): 
-    max_val = max(wave_window)
-    min_val = min(wave_window)
-    stdev = np.std(wave_window)
-    sum_val = sum(wave_window)
-    sum_pos_val = sum([x for x in wave_window if x > 0])
-    sum_abs_val = sum([abs(x) for x in wave_window])
-    return [max_val, min_val, stdev, sum_val, sum_pos_val, sum_abs_val]
+    print("Splitting into windows...", file=sys.stderr)
+    wave_windows = np.array_split(wave, len(wave)/eeg.sample_rate/winsize)
+    # wave_windows = np.array_split(wave, len(wave)/winsize)
 
-  Examples = np.array(map(extract_features, wave_windows))
+    print("Extracting features...", file=sys.stderr)
+    def extract_features(wave_window): 
+      max_val = max(wave_window)
+      min_val = min(wave_window)
+      stdev = np.std(wave_window)
+      sum_val = sum(wave_window)
+      sum_pos_val = sum([x for x in wave_window if x > 0])
+      sum_abs_val = sum([abs(x) for x in wave_window])
+      return [max_val, min_val, stdev, sum_val, sum_pos_val, sum_abs_val]
 
-  print "Training models, assuming no more than", CONTAMINATION, "anomaly..."
-  od = OneClassSVM(nu=CONTAMINATION, kernel='poly', gamma=0.05)
-  od.fit(Examples)
+    Examples = np.array(map(extract_features, wave_windows))
 
-  decisions = od.decision_function(Examples)
-  # print decisions
-  # print max(decisions), min(decisions)
+    print("Training model, assuming no more than", CONTAMINATION, "anomaly...", file=sys.stderr)
+    od = OneClassSVM(nu=CONTAMINATION, kernel='poly', gamma=0.05)
+    od.fit(Examples)
 
-  print "Most likely windows with anomaly:"
-  # find 5 most likely windows, in desc order
-  largest_indices = np.argsort((-np.absolute(decisions)).ravel())[:20]
-  for large_index in largest_indices:
-    # print large_index, large_index * eeg.sample_rate
-    print large_index*winsize/60, "min (score:", decisions[large_index][0], ")"
+    decisions = od.decision_function(Examples)
+    # print decisions
+    # print max(decisions), min(decisions)
 
-find_anomaly("Fp1", "F3", 15)
-find_anomaly("F7", "T3", 15)
-find_anomaly("Fp2", "F4", 15)
+    print("Most likely windows with anomaly:")
+    # find most likely windows, in desc order
+    largest_indices = np.argsort((-np.absolute(decisions)).ravel())[:20]
+    for large_index in largest_indices:
+      print(large_index*winsize/60, "min (score:", decisions[large_index][0], ")")
+
+  find_anomaly("Fp1", "F3", 15)
+  find_anomaly("F7", "T3", 15)
+  find_anomaly("Fp2", "F4", 15)
+
+if __name__ == '__main__':
+  process_file(sys.argv[1])
---
+++ autofind_batch_nk.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+from autofind import process_file
+import sys
+import os
+
+filenames = sys.argv[1:]
+print("Will process:", filenames, file=sys.stderr)
+
+for filename in filenames:
+  print("Working on", filename, file=sys.stderr)
+
+  assert filename.lower().endswith('.eeg')
+  print("Converting .EEG to .edf...", file=sys.stderr)
+  if os.system('nk2edf -no-annotations "' + filename + '"') != 0:
+    print("WARNING: failed to nk2edf...ignoring .EEG file!", file=sys.stderr)
+    continue
+
+  fid = filename[:-4]  # strip .eeg
+  edfname = fid + '_1-1.edf'
+
+  if os.path.isfile(edfname) == False:
+    print("WARNING: can't find .edf file according to convention, skipping...", file=sys.stderr)
+    continue
+
+  try:
+    print('Processing', edfname, '...', file=sys.stderr)
+    process_file(edfname)
+  except Exception as e:
+    print('Exception:', e, file=sys.stderr)
+  finally:
+    print('Deleting temporary .edf file:', edfname, file=sys.stderr)
+    os.remove(edfname)
